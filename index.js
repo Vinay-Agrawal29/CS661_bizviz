3ec6ff864c2be341919e7371e0b98c0329552279
DeepakMathur09 <mathurdeepak09061999@gmail,com>
2023-04-20 21:40:35 +0530

    vis-1 completed except avg cost

--- index.js
+++ index.js
@@ -1,215 +1,205 @@
-// Load the CSV data using D3.js
-d3.csv("./data/downsampled_zomato.csv").then(function(data) {
-  // Define an empty object to hold the column-wise data
+d3.csv("./data/downsampled_zomato.csv").then(function (data) {
   var json = {};
-  // console.log("data",data);
 
-  // Loop through each row of the CSV data
-  data.forEach(function(d) {
-    // Loop through each column of the row
-    Object.keys(d).forEach(function(key) {
-      // If the column doesn't exist in the JSON object, create it
+  data.forEach(function (d) {
+    Object.keys(d).forEach(function (key) {
       if (!json[key]) {
         json[key] = [];
       }
-      // Add the value to the column array
-      json[key].push(d[key]); 
+      json[key].push(d[key]);
     });
   });
 
-  console.log("json keys: ",Object.keys(json));
+  console.log("json keys: ", Object.keys(json));
 
   cuisines = json["cuisines"];
   let targetArray = [];
   console.log(cuisines[0]);
-  for (let i =0; i< cuisines.length;i++){
+  for (let i = 0; i < cuisines.length; i++) {
     let arr = cuisines[i].split(", ");
 
-// Use a for loop to iterate over the elements of the array
-    arr.forEach(function(item) {
-    targetArray.push(item);
+    arr.forEach(function (item) {
+      targetArray.push(item);
     });
   }
-  
 
-const uniqueCount = targetArray.reduce((countMap, str) => {
-  const key = str.toLowerCase().replace(/\s+/g, '');
-  countMap.set(key, (countMap.get(key) || 0) + 1);
-  return countMap;
-}, new Map());
+  const uniqueCount = targetArray.reduce((countMap, str) => {
+    const key = str.toLowerCase().replace(/\s+/g, "");
+    countMap.set(key, (countMap.get(key) || 0) + 1);
+    return countMap;
+  }, new Map());
 
-const sortedUniqueCount = new Map(
-  [...uniqueCount.entries()].sort((a, b) => b[1] - a[1])
-);
+  const sortedUniqueCount = new Map(
+    [...uniqueCount.entries()].sort((a, b) => b[1] - a[1])
+  );
 
-// const top9cuisines = [...sortedUniqueCount.keys()].sort((a, b) => sortedUniqueCount.get(b) - sortedUniqueCount.get(a));
-// const counts = [...sortedUniqueCount.values()];
+  const top9cuisines = [...sortedUniqueCount.keys()].slice(0, 9);
+  const counts = top9cuisines.map((key) => sortedUniqueCount.get(key));
 
-// console.log(top9cuisines);
-// console.log(counts);
-// console.log(uniqueCount);
-const top9cuisines = [...sortedUniqueCount.keys()].slice(0, 9);
-const counts = top9cuisines.map(key => sortedUniqueCount.get(key));
+  let map = new Map();
+  for (j = 0; j < top9cuisines.length; j++) {
+    const count_rate = new Array(4).fill(0);
+    for (let i = 0; i < json["cuisines"].length; i++) {
+      let rate = json["rate"][i];
+      let cuisines = json["cuisines"][i];
+      cuisines = cuisines.split(", ");
 
+      let cuisineExist = cuisines.some((cuisine) => {
+        const cleanedCuisine = cuisine.toLowerCase().replace(/\s+/g, "");
+        return cleanedCuisine == top9cuisines[j];
+      });
+      if (cuisineExist) {
+        console.log("rate",rate.slice(0,3));
+        let rate_float = parseFloat(rate.slice(0,3));
 
-console.log(top9cuisines);
-console.log(counts);  
+        if(rate_float<=3){
+          count_rate[0] += 1;
+        }
+        if(rate_float > 3 && rate_float< 3.5){
+          count_rate[1] += 1;
+        }
+        if(rate_float >= 3.5 && rate_float< 4){
+          count_rate[2] += 1;
+        }
+        if(rate_float >= 4){
+          count_rate[3] += 1;
+        }
 
+        // switch (rate[0]) {
+        //   case "0": {
+        //     count_rate[0] += 1;
+        //     break;
+        //   }
+        //   case "1": {
+        //     count_rate[0] += 1;
+        //     break;
+        //   }
+        //   case "2": {
+        //     count_rate[0] += 1;
+        //     break;
+        //   }
+        //   case "3": {
+        //     count_rate[1] += 1;
+        //     break;
+        //   }
+        //   case "4": {
+        //     count_rate[2] += 1;
+        //     break;
+        //   }
+        //   case "5": {
+        //     count_rate[2] += 1;
+        //     break;
+        //   }
+        //   default:
+        //     console.log("fault index", i);
+        // }
+      }
+    }
+    map.set(top9cuisines[j], count_rate);
+    console.log("count rate: ", count_rate);
+  }
 
+  console.log(map);
 
-// let fix_cuisine = "northindian";
-let map = new Map();
-// console.log(top9cuisines.length,"dfuf");
-for (j = 0; j< top9cuisines.length; j++)
-{
-  const count_rate = new Array(5).fill(0);
-// console.log(json.length(),"total length")
-for (let i=0; i< json["cuisines"].length;i++){
-  // console.log('value of i', i);
-  
-  let rate =  json["rate"][i];
-  let cuisines = json["cuisines"][i];
-  cuisines = cuisines.split(", ");
-  // console.log(cuisines);
+  const newMap = new Map([
+    ["index 0", []],
+    ["index 1", []],
+    ["index 2", []],
+    ["index 3", []],
+  ]);
+  console.log(newMap);
 
-  let cuisineExist = cuisines.some((cuisine) => {
-    const cleanedCuisine = cuisine.toLowerCase().replace(/\s+/g, '');
-    // console.log(cleanedCuisine == fix_cuisine);
-    return cleanedCuisine == top9cuisines[j];
+  for (const [key, arr] of map) {
+    for (let i = 0; i < arr.length; i++) {
+      newMap.get(`index ${i}`).push(arr[i]);
+    }
+  }
+
+  var dom = document.getElementById("chart-container");
+  var myChart = echarts.init(dom, null, {
+    renderer: "canvas",
+    useDirtyRect: false,
   });
-  if (cuisineExist) {
-    // console.log("rate/ cuisines/i",rate, cuisines,i);
-    console.log(rate[0]);
-    switch(rate[0]){
-      
-      case "0": {count_rate[0] += 1;break;}
-      case "1": {count_rate[1] += 1;break;}
-      case "2": {count_rate[2] += 1;break;}
-      case "3": {count_rate[3] += 1;break;}
-      case "4": {count_rate[4] += 1;break;}
-      case "5": {count_rate[4]+= 1;break;}
-      default: console.log("fault index", i);
+  var app = {};
 
-    }
-  } 
-    
-}
-map[top9cuisines[j]]= count_rate;
-console.log("count rate: ",count_rate);
+  var option;
 
-
-}
-
-console.log(Object.keys(map));
-
-const under_1 = new Array(9);
-const under_2 = new Array(9);
-const under_3 = new Array(9);
-const under_4 = new Array(9);
-const under_5 = new Array(9);
-
-Object.keys(map).forEach(function(key) {
-  console.log(key);
-  });  
-
-
-// // Use a for loop to iterate over the elements of the array
-//   arr.forEach(function(item) {
-//   targetArray.push(item);
-//   });
-
-
-
-
-var dom = document.getElementById('chart-container');
-var myChart = echarts.init(dom, null, {
-  renderer: 'canvas',
-  useDirtyRect: false
-});
-var app = {};
-
-var option;
-
-option = {
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'shadow'
-    }
-  },
-  legend: {},
-  grid: {
-    left: '3%',
-    right: '4%',
-    bottom: '3%',
-    containLabel: true
-  },
-  xAxis: [
-    {
-      type: 'category',
-      data:top9cuisines
-    }
-  ],
-  yAxis: [
-    {
-      type: 'value'
-    }
-  ],
-  series: [
-    {
-      name: '<2',
-      type: 'bar',
-      stack: 'Ad',
-      emphasis: {
-        focus: 'series'
+  option = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "shadow",
       },
-      data: [320, 332, 301, 334, 390, 330, 320,89,1]
+    //   formatter: function(params) {
+    //     var data = params[0].data; // Get the original data for the hovered bar
+    //     var list = new Array(3).fill(0);
+    //     var x = 10; // Value of additional variable
+    //     return 'Value: ' + list[0] + '<br>' + list[1]+ '<br>' + 'X: ' + x;
+    // }
     },
-    {
-      name: '2-3.5',
-      type: 'bar',
-      stack: 'Ad',
-      emphasis: {
-        focus: 'series'
+    legend: {},
+    grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: "category",
+        data: top9cuisines,
       },
-      data: [120, 132, 101, 134, 90, 230, 210,89,1]
-    },
-    {
-      name: '>= 3.5',
-      type: 'bar',
-      stack: 'Ad',
-      emphasis: {
-        focus: 'series'
+    ],
+    yAxis: [
+      {
+        type: "value",
       },
-      data: [220, 182, 191, 234, 290, 330, 310,89,1]
-    },
-    {
-            name: 'Video Ads',
-            type: 'bar',
-            stack: 'Ad',
-            emphasis: {
-              focus: 'series'
-            },
-            data: [150, 232, 201, 154, 190, 330, 410,89,1]
-          },
-          {
-            name: 'Search Engine',
-            stack: 'Ad',
-            type: 'bar',
-            data: [862, 1018, 964, 1026, 1679, 1600, 1570,89,1],
-            emphasis: {
-              focus: 'series'
-            }},
-    
-  ]
-};
+    ],
+    series: [
+      {
+        name: "below 3",
+        type: "bar",
+        stack: "Ad",
+        emphasis: {
+          focus: "series",
+        },
+        data: newMap.get("index 0"),
+      },
+      {
+        name: "Between 3 and 3.5",
+        type: "bar",
+        stack: "Ad",
+        label: {show: false,text: "cfy" },
+        emphasis: {
+          focus: "series",
+        },
+        data: newMap.get("index 1"),
+      },
+      {
+        name: "Between 3.5 and 4",
+        type: "bar",
+        stack: "Ad",
+        emphasis: {
+          focus: "series",
+        },
+        data: newMap.get("index 2"),
+      },{
+        name: "Above 4",
+        type: "bar",
+        stack: "Ad",
+        emphasis: {
+          focus: "series",
+        },
+        data: newMap.get("index 3"),
+      },
+    ],
+  };
 
-if (option && typeof option === 'object') {
-  myChart.setOption(option);
-}
+  if (option && typeof option === "object") {
+    myChart.setOption(option);
+  }
 
-window.addEventListener('resize', myChart.resize);
-  
+  window.addEventListener("resize", myChart.resize);
 });
 
 // var dom = document.getElementById('chart-container');
